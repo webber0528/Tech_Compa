a7bb8e3a734948604d866b6c9c310d716cf2baad
nagata <tatsuyan2017@i.softbank.jp>
2023-04-24 08:57:53 +0900

    testcommit

--- resources/views/chats/index.blade.php
+++ resources/views/chats/index.blade.php
@@ -5,6 +5,7 @@
     @foreach($users as $u)
               <a href="/chats/{{$u->id}}" class="alert-link">{{ $u->name }}</a><br/>
               
+              
     @endforeach
         
 </x-app-layout>
